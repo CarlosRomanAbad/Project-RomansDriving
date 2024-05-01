9c6c9ffd0d323ba4825fe0d34465437117dbc683
carlitosromvn <roman.abcar24@triana.salesianos.edu>
2024-05-01 22:52:03 +0200

    mbf

--- RomansDriving/src/main/resources/import.sql
+++ RomansDriving/src/main/resources/import.sql
@@ -1,10 +1,5 @@
--- Insert 1
-INSERT INTO usuario  (id,nombre, dni, apellidos, usuario, email, contrasenha, telefono, tiene_carnet_autoescuela, is_admin) VALUES (1,'Juan', '12345678A', 'Pérez Gómez', 'juanito123', 'juan@example.com', 'contraseña123', '123456789', true, false);
 
--- Insert 2
-INSERT INTO usuario  (id,nombre, dni, apellidos, usuario, email, contrasenha, telefono, tiene_carnet_autoescuela, is_admin) VALUES (2,'María', '87654321B', 'López García', 'marialo', 'maria@example.com', 'clave123', '987654321', false, true);
-
--- Insert 3
+INSERT INTO usuario  (id,nombre, dni, apellidos, usuario, email, contrasenha, telefono, tiene_carnet_autoescuela, is_admin) VALUES (1,'Juan', '12345678A', 'Pérez Gómez', 'juanito123', 'juan@example.com', 'contraseña123', '123456789', true, false)
+INSERT INTO usuario  (id,nombre, dni, apellidos, usuario, email, contrasenha, telefono, tiene_carnet_autoescuela, is_admin) VALUES (2,'María', '87654321B', 'López García', 'marialo', 'maria@example.com', 'clave123', '987654321', false, true)
 INSERT INTO usuario  (id,nombre, dni, apellidos, usuario, email, contrasenha, telefono, tiene_carnet_autoescuela, is_admin) VALUES (3,'Carlos', '11223344C', 'Martinez Rodríguez', 'carlitos87', 'carlos@example.com', 'password123', '1122334455', true, false);
 
--- Insert 4
