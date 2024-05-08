914e5c5f53d0351d144cf15bb595f8a425fac131
Carlos Roman Abad <roman.abcar24@triana.salesianos.edu>
2024-05-08 11:55:29 +0200

    primer cambio

--- RomansDriving/src/main/java/com/salesianostriana/edu/romansdriving/controller/HtmlController.java
+++ RomansDriving/src/main/java/com/salesianostriana/edu/romansdriving/controller/HtmlController.java
@@ -1,9 +1,12 @@
 package com.salesianostriana.edu.romansdriving.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class HtmlController {
 
     @GetMapping("/")
@@ -11,4 +14,11 @@ public class HtmlController {
         return "user/principal";
     }
 
+    @GetMapping("/mostrarPlantillaVehiculo")
+    public String mostrarPlantillaVehiculo(Model model) {
+    	
+    	model.addAttribute("prueba", null);
+    	return "prueba";
+    }
+    
 }
