df627804e4b98db33eea124d39df03ec8fe6bfb5
Carlos Roman Abad <roman.abcar24@triana.salesianos.edu>
2024-05-17 12:28:40 +0200

    mejoro seguridad

--- RomansDriving/src/main/java/com/salesianostriana/edu/romansdriving/security/SecurityConfig.java
+++ RomansDriving/src/main/java/com/salesianostriana/edu/romansdriving/security/SecurityConfig.java
@@ -53,7 +53,9 @@ SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(
             (authz) -> authz
 			.requestMatchers("/admin/**"/*"/h2-console/**"*/).hasRole("ADMIN")
-					.requestMatchers("/PlantillaClasesVehiculo/**","/PlantillaClasesCoche/**","/PlantillaClasesMoto/**","/PlantillaClasesCamion/**").hasRole("USER")
+					.requestMatchers("/PlantillaClasesVehiculo/**","/PlantillaClasesCoche/**"
+							,"/PlantillaClasesMoto/**","/PlantillaClasesCamion/**","/reserva/**")
+					.hasRole("USER")
                     .anyRequest().permitAll()) 
             .formLogin((loginz) -> loginz
                     .loginPage("/login").defaultSuccessUrl("/").permitAll())
