3fa1e683fbd3082db6adadaaf2d8707c9ed89a59
carlitosromvn <roman.abcar24@triana.salesianos.edu>
2024-06-06 22:26:13 +0200

    final commit

--- RomansDriving/src/main/java/com/salesianostriana/edu/romansdriving/controller/AdminController.java
+++ RomansDriving/src/main/java/com/salesianostriana/edu/romansdriving/controller/AdminController.java
@@ -6,11 +6,13 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.salesianostriana.edu.romansdriving.model.Clase;
@@ -22,7 +24,7 @@ import com.salesianostriana.edu.romansdriving.service.ProfesorService;
 import com.salesianostriana.edu.romansdriving.service.UsuarioService;
 import com.salesianostriana.edu.romansdriving.service.VehiculoService;
 
-a
+
 
 @Controller
 @RequestMapping("/admin")
